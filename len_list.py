5b0717ae89f20ebee40cb3e6a3d57f388ee24b13
kritu08231 <162892864+kritu08231@users.noreply.github.com>
2025-02-25 18:59:32 +0530

    Create len_list.py

---
+++ len_list.py
@@ -0,0 +1,12 @@
+def cal_len(a:[]):
+    print(len(a))
+
+cal_len([1,5,4,7,5,6,8])
+
+#WAF to print the elements of a list in a single line.
+
+def single_line(a:[]):
+    for items in a:
+        print(items, end=" ")
+
+single_line([4,5,7,8,2,4,1,4])
